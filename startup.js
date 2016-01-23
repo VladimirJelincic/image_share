044b3dbd0fc7927ac92177973d756c564c44854d
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2016-01-23 20:56:22 +0100

    Initial commit

---
+++ startup.js
@@ -0,0 +1,24 @@
+/**
+ * Created by vladimir on 23/01/16.
+ */
+console.log("startup.js");
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (Images.find().count() == 0) {
+
+            for(var i=1;i<23;i++){
+                Images.insert(
+                    {
+                        img_src: "img_"+i+".jpg",
+                        img_alt: "image number "+i
+
+                    }
+                );
+            }
+
+            console.log("Images:" + Images.find().count());
+
+        }
+    })
+
+}
